ddf5f7389e69c5fa063a9f8ad8b6cce0e417e614
Рома <skillthorn@gmail.com>
2021-03-26 17:05:21 +0200

    footer

--- wp-content/themes/mlm/footer-blog.php
+++ wp-content/themes/mlm/footer-blog.php
@@ -1,6 +1,88 @@
 <?php wp_footer(); ?>
+<footer class="border-top-default active">
+    <div class="footer-toprow">
+        <div class="container">
+            <div class="left-col flex">
+                <div class="logo">
+                    <img alt="UNEED PARTNERS GROUP" src="http://sergiy.takasho.work/mlm/wp-content/themes/mlm/assets/img/logo.svg">
+                </div>
+                <div class="phone">
+                    <span>Номер для консультации</span>
 
+                    <a href="tel:380939412125" class="bebas-bold">380939412125</a>
+
+                </div>
+            </div>
+            <div class="right-col w-100 flex">
+                <ul class="nav-list">
+                    <li>
+                        <a href="http://sergiy.takasho.work/mlm/#">Пакеты</a>
+                    </li>
+                    <li>
+                        <a href="http://sergiy.takasho.work/mlm/#">Про компанию</a>
+                    </li>
+                    <li>
+                        <a href="http://sergiy.takasho.work/mlm/#">Контакты</a>
+                    </li>
+                    <li>
+                        <a href="#">Блог</a>
+                    </li>
+                </ul>
+                <div class="phone">
+                    <span>Номер для консультации</span>
+                    <a href="tel:380939412123" class="bebas-bold">380939412123</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-botrow border-top-default">
+        <div class="container">
+            <div class="left-col flex">
+                <ul>
+                    <li>
+                        <a href="http://sergiy.takasho.work/mlm/privacy">
+                            Privacy Policy
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://sergiy.takasho.work/mlm/cookies">
+                            Cookies Policy
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://sergiy.takasho.work/mlm/terms">
+                            Terms and Conditions
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="right-col w-100 flex">
+                <ul>
+                    <li>
+                        <a href="#" class="link-default">Instagram</a>
+                    </li>
+                    <li>
+                        <a href="#" class="link-default">Linkedin</a>
+                    </li>
+                    <li>
+                        <a href="#" class="link-default">Facebook</a>
+                    </li>
+                </ul>
+                <div class="credit-cards">
+                    <img alt="" src="http://sergiy.takasho.work/mlm/wp-content/themes/mlm/assets/img/visa.svg">
+                    <img alt="" src="http://sergiy.takasho.work/mlm/wp-content/themes/mlm/assets/img/mastercard.svg">
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
 </div>
-
 </body>
+<script>
+    $('.nav-btn').click(function () {
+        $(this).toggleClass('active');
+        $('body').toggleClass('overflow-h');
+        $('.nav-menu').toggleClass('active');
+    });
+</script>
 </html>
